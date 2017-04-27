cc9826de50868efe28c2e3ad64714f21ddc37dee
Alex Law <achlawuk@gmail.com>
2017-04-27 12:43:38 +0100

    Working attack and hit points reduction

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,11 @@
+require "player"
+require "./app"
+
 feature 'Attack' do
   scenario 'Player 1 attacks Player 2' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to have_content('Bertie attacked Alex')
+    expect{ $player2.receive_damage }.to change{ $player2.hit_points }.by(-10)
   end
 end
